d326f21c283af06594539e13397cc83b672c6455
GrieferAtWork <GrieferAtWork@users.noreply.github.com>
2021-10-06 16:53:34 +0200

    Add test to reproduce bug #2

--- test/_runall.sh
+++ test/_runall.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
 
-cd "$(dirname "$(readlink -f "$0")")"
-TPP="./../bin/tpp"
+cd "$(dirname "$(readlink -f "$0")")/.."
+TPP="$(pwd)/bin/tpp"
 if [[ -f "${TPP}.exe" ]]; then TPP="${TPP}.exe"; fi
 TPP="$TPP -P -Werror"
 
+# testFile <filename>
+testFile() {
+	echo "Running: $TPP -fno-spc -fno-lf $1"
+	$TPP -fno-spc -fno-lf "$1" || {
+		error="$?"
+		echo "Command failed: '$TPP -fno-spc -fno-lf $1' -> '$error'"
+		exit $error
+	}
+	echo "Running: $TPP $1"
+	$TPP "$1" > /dev/null || {
+		error="$?"
+		echo "Command failed: '$TPP $1' -> '$error'"
+		exit $error
+	}
+}
+
+cd "test"
 for file in *.h; do
-	echo "Running: $TPP -fno-spc -fno-lf $file"
-	$TPP -fno-spc -fno-lf $file || {
-		error="$?"
-		echo "Command failed: '$TPP -fno-spc -fno-lf $file' -> '$error'"
-		exit $error
-	}
-	echo "Running: $TPP $file"
-	$TPP $file > /dev/null || {
-		error="$?"
-		echo "Command failed: '$TPP $file' -> '$error'"
-		exit $error
-	}
+	testFile "$file"
 done
+
+# This test requires some special care as it relates to relative includes
+cd inner
+testFile "test.h"
+cd ..
